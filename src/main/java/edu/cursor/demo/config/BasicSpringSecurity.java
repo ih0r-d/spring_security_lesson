6f0e0d2d8e0966ae3d9de2e3bf26c94d1135908c
Ihor Didyk <ihordidyk95@gmail.com>
2019-08-21 21:42:11 +0300

    added example of basic spring security config

--- src/main/java/edu/cursor/demo/config/BasicSpringSecurity.java
+++ src/main/java/edu/cursor/demo/config/BasicSpringSecurity.java
@@ -1,38 +1,84 @@
 package edu.cursor.demo.config;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class BasicSpringSecurity extends WebSecurityConfigurerAdapter {
 
+    @Qualifier("userServiceImpl")
+    @Autowired
+    private UserDetailsService userService;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth)
-            throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user")
-                .password("password")
-                .roles("USER")
-                .and()
-                .withUser("admin")
-                .password("admin")
-                .roles("USER", "ADMIN");
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userService);
+        return provider;
     }
+
+    private InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder> inMemoryConfigure() {
+        return new InMemoryUserDetailsManagerConfigurer<>();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder builder,
+                                AuthenticationProvider provider) throws Exception {
+        inMemoryConfigure()
+                .withUser("ss")
+                .password("ss")
+                .roles("ADMIN")
+                .and()
+                .configure(builder);
+        builder.authenticationProvider(provider);
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-               .authorizeRequests()
-                .antMatchers( "users/").permitAll()
-                .antMatchers( "admin/**").hasRole("ADMIN")
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/", "/**").permitAll()
+                .antMatchers("/admin/**").hasAnyRole("ADMIN")
+                .antMatchers("/users/**").hasAnyRole("USER")
                 .anyRequest().authenticated();
+
+        http.formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/j_spring_security_check")
+                .failureUrl("/login?error")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .permitAll();
+
+        http.logout()
+                .permitAll()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout")
+                .invalidateHttpSession(true);
+
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
